1f988e2368f438695a789e0394a20b5efee2b369
Esmee-Cassee <esmeecassee@icloud.com>
2024-11-15 14:34:37 +0100

    added the option
    I added the option with which I can choose the amount of steps in between 0 and 2pi.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -8,12 +8,22 @@ def start_list():
     pass
 
 @start_list.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 @start_list.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
