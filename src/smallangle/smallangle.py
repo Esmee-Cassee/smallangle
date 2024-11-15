372a8b02c24a28af7b8b798a1d68e77db80ff187
Esmee-Cassee <esmeecassee@icloud.com>
2024-11-15 14:32:36 +0100

    removed function start_list
    I changed the script so that the function doesn't contain all the code anymore, now It just contains pass and with the sub commands we can use sin and tan.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,19 +3,21 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
 def start_list():
-    def sin(number):
-        x = np.linspace(0, 2 * pi, number)
-        df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
-        print(df)
+    pass
 
+@start_list.command()
+def sin(number):
+    x = np.linspace(0, 2 * pi, number)
+    df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
+    print(df)
 
-    def tan(number):
-        x = np.linspace(0, 2 * pi, number)
-        df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
-        print(df)
+@start_list.command()
+def tan(number):
+    x = np.linspace(0, 2 * pi, number)
+    df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
+    print(df)
 
-
-    if __name__ == "__main__":
-        sin(10)
-
+if __name__ == "__main__":
+    start_list()
